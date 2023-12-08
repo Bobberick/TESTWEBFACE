e7040916e4f1df52cbf3d394d9df695c618f0d61
LAPTOP-L92O5S0V\Ps <noobphc@gmail.com>
2023-12-08 10:04:19 +0700

    TEST

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/public/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/public/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/public/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/public/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -25,7 +25,5 @@ video.addEventListener('play', () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
